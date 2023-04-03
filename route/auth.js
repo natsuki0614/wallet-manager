f318ec7fd92fb3b8da3550d6a890a1ad6b4a8f1d
natsuki <n.shino0614@gmail.com>
2023-04-03 15:37:50 +0900

    install bcryptjs

--- route/auth.js
+++ route/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const connection = require('./dbConnect.js');
 
